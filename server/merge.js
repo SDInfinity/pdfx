6b1bc297c7ac02369d7a2ac2a6438e863835c2ef
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-04-29 11:49:44 +0530

    First commit.

---
+++ server/merge.js
@@ -0,0 +1,20 @@
+import pdfMerger from "pdf-merger-js";
+import fs from "fs";
+
+const merger = new pdfMerger();
+
+export const pdfmerger = async (p1,p2)=>{
+        const mergedFilePath = 'public/merged.pdf';
+
+  // Remove the previously merged file
+  if (fs.existsSync(mergedFilePath)) {
+    fs.unlinkSync(mergedFilePath);
+  }
+  await merger.add(p1); // Add the first PDF
+  await merger.add(p2); // Add the second PDF
+
+  await merger.save(mergedFilePath); // Save the merged PDF
+
+  merger.reset();
+};
+
---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path from "path";
+import multer from "multer";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import {pdfmerger} from "./merge.js";
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname,"templates")));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "templates/index.html"));
+  });
+
+
+app.post("/merge", upload.array("pdfs", 2), async (req, res, next) => {
+
+        console.log(req.files);
+        //run the padfmerger function thats takes two pdfs
+        await pdfmerger(path.join(req.files[0].path), path.join(req.files[1].path));
+        res.sendFile(path.join(__dirname, "public/merged.pdf"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
